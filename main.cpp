c2b015995d01b4eb2b47cd483504fd8fa7d34e76
suzuki <k025g0024@g.neec.ac.jp>
2026-04-14 16:11:42 +0900

    プロジェクトファイルを追加。

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<windows.h>
+
+//windowsアプリでエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE,_In_opt_ HINSTANCE,_In_ LPSTR,_In_ int )
+{
+	//出力ウィンドウへの文字出力
+	OutputDebugStringA("Hello,DirectX!\n");
+
+	return 0;
+}
